b793c8b1109a5df568fcf00e7cd36444343010f5
Willy Mene <wmene@stanford.edu>
2014-10-29 13:46:50 -0700

    Initial commit

---
+++ config/deploy/staging.rb
@@ -0,0 +1,5 @@
+set :deploy_host, ask("Server", 'enter in the server you are deploying to. do not include .stanford.edu')
+server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, "development"
