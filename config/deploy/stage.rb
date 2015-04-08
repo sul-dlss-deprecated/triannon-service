7e324d6c17e32b37fee8027e858f19faedba890e
Naomi Dushay <ndushay@stanford.edu>
2015-04-07 17:25:45 -0700

    capistrano sets rails_env of stage to 'production'

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,3 +2,4 @@ ask(:deploy_host, 'Enter in the server you are deploying to. do not include .sta
 server "#{fetch(:deploy_host)}.stanford.edu", user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, "production"
